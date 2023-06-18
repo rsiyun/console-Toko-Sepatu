e812db1c535097c62c09e2dd0596efed58e715dd
Raihan Siyun <rsiyun@gmail.com>
2023-06-18 10:31:29 +0700

    add structure folder

--- src/App.java
+++ src/App.java
@@ -1,15 +1,20 @@
 import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import classPackage.User;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Conn connection = new Conn();
-        
         String sql = "SELECT * FROM users";
         ResultSet rs = connection.getConnection().createStatement().executeQuery(sql);
+        ArrayList<User> userList = new ArrayList<User>();
         while(rs.next()){
-            System.out.println("ID cok: " + rs.getInt("id_user"));
-            System.out.println("uwong: " + rs.getString("username"));
-            System.out.println("role: " + rs.getString("role"));
+            User user = new User(rs.getInt("id_user"), rs.getString("username"), rs.getString("password"), rs.getInt("role"), rs.getInt("active"));
+            userList.add(user);
+        }
+        for (int i = 0; i < userList.size(); i++) {
+            System.out.print(userList.get(i).getId_user()+ "---" +userList.get(i).getUsername() + "---" +userList.get(i).getPassword());
         }
     }
 }
---
+++ src/entity/User.java
@@ -0,0 +1,46 @@
+package classPackage;
+
+public class User {
+    private int id_user;
+    private String username;
+    private String password;
+    private int role;
+    private int active;
+    public User(int id_user, String username, String password, int role, int active) {
+        this.id_user = id_user;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.active = active;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public int getRole() {
+        return role;
+    }
+    public void setRole(int role) {
+        this.role = role;
+    }
+    public int getActive() {
+        return active;
+    }
+    public void setActive(int active) {
+        this.active = active;
+    }
+    public int getId_user() {
+        return id_user;
+    }
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
+    }
+}
