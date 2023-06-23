d2d5de251f452b25475b7dd86c901fcc7a0a6c9f
Raihan Siyun <rsiyun@gmail.com>
2023-06-23 20:29:21 +0700

    for awan transaksi

--- src/entity/Transaksi.java
+++ src/entity/Transaksi.java
@@ -7,15 +7,13 @@ public class Transaksi {
     private int idUser;
     private float totalHarga;
     private Date tglTransaksi;
-    private int stock;
     private User user;
 
-    public Transaksi(int idTransaksi, int idUser, float totalHarga, Date tglTransaksi, int stock, User user) {
+    public Transaksi(int idTransaksi, int idUser, float totalHarga, Date tglTransaksi, User user) {
         this.idTransaksi = idTransaksi;
         this.idUser = user.getIdUser();
         this.totalHarga = totalHarga;
         this.tglTransaksi = tglTransaksi;
-        this.stock = stock;
         this.user = user;
     }
 
@@ -39,12 +37,6 @@ public class Transaksi {
     }
     public void setTglTransaksi(Date tglTransaksi) {
         this.tglTransaksi = tglTransaksi;
-    }
-    public int getStock() {
-        return stock;
-    }
-    public void setStock(int stock) {
-        this.stock = stock;
     }
     public User getUser() {
         return user;
--- src/service/AllSql.java
+++ src/service/AllSql.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import entity.Brand;
 import entity.Produk;
 import entity.ProdukDetail;
+import entity.Transaksi;
+import entity.User;
 
 public class AllSql extends ExecuteSql{
     // Brand
@@ -41,6 +43,17 @@ public class AllSql extends ExecuteSql{
             Brand brand = new Brand(rs.getInt("id_brand"), rs.getString("brand"));
             Produk produk = new Produk(rs.getInt("id_produk"), rs.getInt("id_brand"), rs.getString("nama_product"), rs.getFloat("harga"), brand);
             list.add(produk);
+        }
+        return list;
+    }
+    public ArrayList<Transaksi> selectTransaksi() throws Exception{
+        String sql = "SELECT id_transaksi, transaksi.id_user, total_harga, tgl_transaksi, users.username, users.active FROM transaksi INNER JOIN users ON transaksi.id_user = users.id_user;";
+        ResultSet rs = this.sqlquerry(sql);
+        ArrayList<Transaksi> list = new ArrayList<Transaksi>(null);
+        while (rs.next()) {
+            User user = new User(rs.getInt("id_user"), rs.getString("username"), "", 0, rs.getInt("active"));
+            Transaksi transaksi = new Transaksi(rs.getInt("id_transaksi"), rs.getInt("id_user"), rs.getFloat("total_harga"), rs.getDate("tgl_transaksi"), user);
+            list.add(transaksi);
         }
         return list;
     }
